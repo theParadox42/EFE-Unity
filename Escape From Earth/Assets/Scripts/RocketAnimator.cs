f3a4e90cc7051467c62725414b2b029cc5f4df2c
Nathaniel Fargo <nathaniel.fargo@gmail.com>
2020-01-14 18:23:32 -0700

    Initial Commit

---
+++ Escape From Earth/Assets/Scripts/RocketAnimator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RocketAnimator : MonoBehaviour
+{
+
+    [SerializeField] Sprite[] rocketOnFrames = {};
+    [SerializeField] Sprite[] rocketOffFrames = {};
+    [SerializeField] float framesPerSecond = 10f;
+    [SerializeField] bool rocketOn = true;
+    Image myImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myImage = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Sprite[] spritesToUse = rocketOn ? rocketOnFrames : rocketOffFrames;
+        int frameIndex = Mathf.RoundToInt(Time.time * framesPerSecond) % spritesToUse.Length;
+        myImage.overrideSprite = spritesToUse[frameIndex];
+    }
+}
