9d092f612aa8ad3fdf1d317e6702fcf2f7469414
Nathaniel Fargo <nathaniel.fargo@gmail.com>
2020-01-15 18:05:38 -0700

    Started 1st Cutscene

---
+++ Escape From Earth/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Dialogue")]
+public class Dialogue : ScriptableObject {
+
+    [TextArea(10,14)] [SerializeField] string dialogueText;
+    [SerializeField] Dialogue nextDialogue;
+
+    public string GetDialogueText(){
+        return dialogueText;
+    }
+    public Dialogue GetNextDialogue(){
+        return nextDialogue;
+    }
+}
---
+++ Escape From Earth/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void LoadLevelBuilder()
+    {
+
+    }
+    public void LoadCommunityLevels()
+    {
+        SceneManager.LoadScene("Level Select");
+    }
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void LoadSpecificScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
