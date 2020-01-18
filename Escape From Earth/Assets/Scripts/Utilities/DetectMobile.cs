9cb39a33fdd57119a142a383096904acba4a1705
Nathaniel Fargo <nathaniel.fargo@gmail.com>
2020-01-18 14:42:34 -0700

    Rearranged a bunch of stuff

---
+++ Escape From Earth/Assets/Scripts/Utilities/DetectMobile.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectMobile : MonoBehaviour
+{
+    
+    public bool GetMobile() {
+        #if UNITY_IOS
+
+        #elif UNITY_
+    }
+
+}
